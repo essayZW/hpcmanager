b5331b120b6bd28763d4a1c0907be43c5d8349ff
essay <essaybeihai@gmail.com>
2022-02-06 15:29:22 +0800

    完善日志工具类

--- logger/logger.go
+++ logger/logger.go
@@ -3,10 +3,19 @@ package logger
 import (
 	"fmt"
 	"os"
+	"sync"
+	"time"
 
 	z "github.com/asim/go-micro/plugins/logger/zap/v4"
 	"go-micro.dev/v4/logger"
 	"go.uber.org/zap"
+)
+
+var (
+	cachedLogger logger.Logger
+	cachedError  error
+	createDate   string
+	mutex        sync.Mutex
 )
 
 // New 创建一个新的logger
@@ -15,6 +24,14 @@ import (
 // 日志文件按照日期进行分片,文件名为日期信息
 // 日志的实现为zap库
 func New(name string) (logger.Logger, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	// 判断原来的logger是否已经过期
+	today := time.Now().Format("2006-01-02")
+	if today == createDate {
+		return cachedLogger, cachedError
+	}
 	var zapConfig zap.Config
 	var production string
 	if production = os.Getenv("PRODUCTION"); production != "" {
@@ -33,20 +50,91 @@ func New(name string) (logger.Logger, error) {
 		}
 	}
 	// 添加日志输出到文件
-	zapConfig.OutputPaths = append(zapConfig.OutputPaths, logDir+"/log.txt")
+	filename := fmt.Sprintf("/log-%s.txt", today)
+	zapConfig.OutputPaths = append(zapConfig.OutputPaths, logDir+filename)
 	// 需要通过Options设置日志等级
 	if production == "" {
-		return z.NewLogger(
+		cachedLogger, cachedError = z.NewLogger(
 			z.WithConfig(zapConfig),
 			logger.WithLevel(logger.DebugLevel),
 		)
+	} else {
+		cachedLogger, cachedError = z.NewLogger(
+			z.WithConfig(zapConfig),
+		)
 	}
-	return z.NewLogger(
-		z.WithConfig(zapConfig),
-	)
+	createDate = today
+	return cachedLogger, cachedError
 }
 
 func pathExists(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil
+}
+
+var serviceName string = "log"
+
+// SetName 设置输出日志的服务名称
+func SetName(name string) {
+	serviceName = name
+}
+
+// Debug debug日志
+func Debug(msg ...interface{}) {
+	log, _ := New(serviceName)
+	log.Log(logger.DebugLevel, msg...)
+}
+
+// Debugf 格式化输出debug日志
+func Debugf(format string, v ...interface{}) {
+	log, _ := New(serviceName)
+	log.Logf(logger.DebugLevel, format, v...)
+}
+
+// Info info日志
+func Info(msg ...interface{}) {
+	log, _ := New(serviceName)
+	log.Log(logger.InfoLevel, msg...)
+}
+
+// Infof 格式化输出debug日志
+func Infof(format string, v ...interface{}) {
+	log, _ := New(serviceName)
+	log.Logf(logger.InfoLevel, format, v...)
+}
+
+// Warn warn日志
+func Warn(msg ...interface{}) {
+	log, _ := New(serviceName)
+	log.Log(logger.WarnLevel, msg...)
+}
+
+// Warnf 格式化输出Warn日志
+func Warnf(format string, v ...interface{}) {
+	log, _ := New(serviceName)
+	log.Logf(logger.WarnLevel, format, v...)
+}
+
+// Error error 日志
+func Error(msg ...interface{}) {
+	log, _ := New(serviceName)
+	log.Log(logger.ErrorLevel, msg...)
+}
+
+// Errorf 格式化输出error日志
+func Errorf(format string, v ...interface{}) {
+	log, _ := New(serviceName)
+	log.Logf(logger.ErrorLevel, format, v...)
+}
+
+// Fatal fatal日志
+func Fatal(msg ...interface{}) {
+	log, _ := New(serviceName)
+	log.Log(logger.FatalLevel, msg...)
+}
+
+// Fatalf 格式化输出fatal日志
+func Fatalf(format string, v ...interface{}) {
+	log, _ := New(serviceName)
+	log.Logf(logger.FatalLevel, format, v...)
 }
--- logger/logger_test.go
+++ logger/logger_test.go
@@ -6,7 +6,7 @@ import (
 	"go-micro.dev/v4/logger"
 )
 
-func TestLog(t *testing.T) {
+func TestNew(t *testing.T) {
 	log, err := New("test")
 	if err != nil {
 		t.Error(err)
@@ -16,4 +16,16 @@ func TestLog(t *testing.T) {
 	log.Log(logger.InfoLevel, "test", 123, 789)
 	log.Log(logger.WarnLevel, "test", 123, 789)
 	log.Log(logger.ErrorLevel, "test", 123, 789)
+}
+
+func TestLog(t *testing.T) {
+	SetName("test")
+	Debug("logtest", 1)
+	Debugf("debug: %v", 2)
+	Info("logtest", 3)
+	Infof("info: %v", 4)
+	Warn("logtest", 5)
+	Warnf("warn: %v", 6)
+	Error("logtest", 7)
+	Errorf("error: %v", 8)
 }
