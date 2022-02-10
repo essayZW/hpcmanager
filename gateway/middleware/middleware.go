1304d43faa99bee553eca5af6812ddf58ea65e82
essay <essaybeihai@gmail.com>
2022-02-10 14:46:06 +0800

    构建网关初步鉴权逻辑

--- gateway/middleware/middleware.go
+++ gateway/middleware/middleware.go
@@ -5,13 +5,20 @@ import (
 	"go-micro.dev/v4/client"
 )
 
+var verifyMiddleware *verify
+
 // Registry 注册中间件
 func Registry(router *gin.RouterGroup, client client.Client) {
 	router.Use(gin.Recovery())
 	router.Use(baseReq)
 
-	v := newVerify(client)
-	router.Use(v.HandlerFunc)
+	verifyMiddleware = newVerify(client)
+	router.Use(verifyMiddleware.HandlerFunc)
 
 	router.Use(log)
+}
+
+// RegistryExcludeAPIPath 向初步鉴权中间件注册不需要验证的API地址
+func RegistryExcludeAPIPath(path string) {
+	verifyMiddleware.registryExcludeAPIPath(path)
 }
--- gateway/middleware/verify.go
+++ gateway/middleware/verify.go
@@ -1,22 +1,79 @@
 package middleware
 
 import (
+	"context"
+	"errors"
+
+	"github.com/essayZW/hpcmanager/gateway/proto"
+	"github.com/essayZW/hpcmanager/gateway/response"
 	userpb "github.com/essayZW/hpcmanager/user/proto"
 	"github.com/gin-gonic/gin"
 	"go-micro.dev/v4/client"
+	"go-micro.dev/v4/logger"
 )
+
+// TokeiCookieName  token存储在cookie中的键值
+const TokeiCookieName = "TOKEN"
 
 type verify struct {
 	userService userpb.UserService
+	excludeAPI  []string
 }
 
 // HandlerFunc 进行初步的权限信息以及用户信息获取
 func (v *verify) HandlerFunc(ctx *gin.Context) {
+	// TODO 检查当前请求的接口是否需要用户提供token
+	if !v.needVerify(ctx.Request.RequestURI) {
+		ctx.Next()
+		return
+	}
+	// 获取用户的token信息
+	token, err := ctx.Cookie(TokeiCookieName)
+	if err != nil {
+		resp := response.New(403, errors.New("forbidden! need token"), false, "forbidden! need token")
+		resp.Send(ctx)
+		return
+	}
+	info, err := v.userService.CheckLogin(context.Background(), &userpb.CheckLoginRequest{
+		Token: token,
+	})
+	if err != nil {
+		logger.Error(err)
+		resp := response.New(500, err, false, err.Error())
+		resp.Send(ctx)
+		return
+	}
+	if !info.Login {
+		resp := response.New(403, errors.New("forbidden! need token"), false, "forbidden! need token")
+		resp.Send(ctx)
+		return
+	}
+	value, _ := ctx.Get(BaseRequestKey)
+	breq := value.(*proto.BaseRequest)
+	breq.UserInfo = &proto.UserInfo{
+		Levels:  info.GetPermissionLevel(),
+		UserId:  info.GetUserInfo().GetId(),
+		GroupId: info.GetUserInfo().GetGroupId(),
+	}
 	ctx.Next()
+}
+
+func (v *verify) registryExcludeAPIPath(path string) {
+	v.excludeAPI = append(v.excludeAPI, path)
+}
+
+func (v *verify) needVerify(path string) bool {
+	for index := range v.excludeAPI {
+		if v.excludeAPI[index] == path {
+			return false
+		}
+	}
+	return true
 }
 
 func newVerify(client client.Client) *verify {
 	return &verify{
 		userService: userpb.NewUserService("user", client),
+		excludeAPI:  make([]string, 0),
 	}
 }
