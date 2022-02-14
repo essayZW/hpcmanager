fee23454fae7be5c2b6589ff5f06222a9e0e844b
essay <essaybeihai@gmail.com>
2022-02-14 17:44:52 +0800

    添加其余的数据库表

--- mysql.sql
+++ mysql.sql
@@ -40,6 +40,27 @@ CREATE TABLE `group` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `hpc_usagetime`
+--
+
+DROP TABLE IF EXISTS `hpc_usagetime`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hpc_usagetime` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `queue_name` varchar(64) NOT NULL,
+  `wall_time` int NOT NULL DEFAULT '0',
+  `gwall_time` int NOT NULL DEFAULT '0',
+  `start_time` timestamp NOT NULL,
+  `end_time` timestamp NOT NULL,
+  `create_time` timestamp NOT NULL,
+  `extraAttributes` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `hpc_user`
 --
 
@@ -53,6 +74,107 @@ CREATE TABLE `hpc_user` (
   `node_max_quota` tinyint DEFAULT '1',
   `quota_start_time` timestamp NULL DEFAULT NULL,
   `quota_end_time` timestamp NULL DEFAULT NULL,
+  `extraAttributes` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `node_apply`
+--
+
+DROP TABLE IF EXISTS `node_apply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `node_apply` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `create_time` timestamp NOT NULL,
+  `creater_id` int NOT NULL,
+  `tutor_id` int NOT NULL,
+  `project_id` int NOT NULL,
+  `tutor_check_status` tinyint NOT NULL DEFAULT '-1',
+  `manager_check_status` tinyint NOT NULL DEFAULT '-1',
+  `status` tinyint NOT NULL DEFAULT '1',
+  `message_tutor` varchar(300) DEFAULT NULL,
+  `message_manager` varchar(300) DEFAULT NULL,
+  `tutor_check_time` timestamp NULL DEFAULT NULL,
+  `tutor_checker_id` int DEFAULT NULL,
+  `tutor_checker_name` varchar(32) DEFAULT NULL,
+  `manager_check_time` timestamp NULL DEFAULT NULL,
+  `manager_checker_id` int DEFAULT NULL,
+  `manager_checker_name` varchar(32) DEFAULT NULL,
+  `modify_time` timestamp NULL DEFAULT NULL,
+  `modify_userid` int DEFAULT NULL,
+  `node_type` varchar(64) DEFAULT NULL,
+  `node_num` int DEFAULT NULL,
+  `start_time` timestamp NULL DEFAULT NULL,
+  `end_time` timestamp NULL DEFAULT NULL,
+  `extraAttributes` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `node_distribute`
+--
+
+DROP TABLE IF EXISTS `node_distribute`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `node_distribute` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `apply_id` int NOT NULL,
+  `handler_flag` tinyint NOT NULL DEFAULT '0',
+  `pay_flag` tinyint NOT NULL DEFAULT '0',
+  `extraAttributes` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `node_distribute_log`
+--
+
+DROP TABLE IF EXISTS `node_distribute_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `node_distribute_log` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `queue_name` varchar(64) NOT NULL,
+  `user_id` int NOT NULL,
+  `distribute_id` int NOT NULL,
+  `nodes_names` varchar(512) NOT NULL,
+  `operation` tinyint NOT NULL,
+  `create_time` timestamp NOT NULL,
+  `extraAttributes` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `paper_apply`
+--
+
+DROP TABLE IF EXISTS `paper_apply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `paper_apply` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `creater_id` int NOT NULL,
+  `create_time` timestamp NOT NULL,
+  `tutor_id` timestamp NOT NULL,
+  `paper_title` varchar(256) NOT NULL,
+  `paper_category` varchar(128) NOT NULL,
+  `paper_partition` varchar(32) NOT NULL,
+  `papaer_firstpage_img` varchar(512) NOT NULL,
+  `paper_thankspage_img` varchar(512) NOT NULL,
+  `remark_message` varchar(512) DEFAULT NULL,
+  `check_status` tinyint DEFAULT '-1',
+  `checker_id` int DEFAULT NULL,
+  `checker_name` varchar(32) DEFAULT NULL,
+  `check_money` decimal(18,2) DEFAULT '0.00',
+  `check_message` varchar(512) DEFAULT NULL,
+  `check_time` timestamp NULL DEFAULT NULL,
   `extraAttributes` varchar(2048) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -75,6 +197,89 @@ CREATE TABLE `permission` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `level_UNIQUE` (`level`),
   UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `project` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `from` varchar(128) NOT NULL,
+  `numbering` varchar(128) NOT NULL,
+  `expenses` varchar(16) NOT NULL,
+  `description` varchar(1024) DEFAULT NULL,
+  `creater_user_id` int NOT NULL,
+  `create_time` timestamp NOT NULL,
+  `modify_time` timestamp NOT NULL,
+  `modify_user_id` int NOT NULL,
+  `extraAttributes` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `quota_apply`
+--
+
+DROP TABLE IF EXISTS `quota_apply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `quota_apply` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `user_name` varchar(32) NOT NULL,
+  `user_username` varchar(32) NOT NULL,
+  `oper_type` tinyint NOT NULL,
+  `create_time` timestamp NOT NULL,
+  `check_status` tinyint NOT NULL DEFAULT '-1',
+  `checker_id` int DEFAULT NULL,
+  `checker_name` varchar(32) DEFAULT NULL,
+  `old_size` tinyint DEFAULT NULL,
+  `new_size` tinyint DEFAULT NULL,
+  `new_end_time` timestamp NULL DEFAULT NULL,
+  `delay_year` tinyint DEFAULT NULL,
+  `old_end_time` timestamp NULL DEFAULT NULL,
+  `old_start_time` timestamp NULL DEFAULT NULL,
+  `pay_flag` tinyint DEFAULT '0',
+  `pay_time` timestamp NULL DEFAULT NULL,
+  `pay_type` tinyint DEFAULT NULL,
+  `pay_message` varchar(300) DEFAULT NULL,
+  `extraAttributes` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `technology_apply`
+--
+
+DROP TABLE IF EXISTS `technology_apply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `technology_apply` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `creater_id` int NOT NULL,
+  `create_time` timestamp NOT NULL,
+  `tutor_id` int NOT NULL,
+  `project_id` int NOT NULL,
+  `project_name` varchar(128) NOT NULL,
+  `project_description` varchar(1024) DEFAULT NULL,
+  `prize_level` varchar(128) NOT NULL,
+  `prize_img` varchar(512) NOT NULL,
+  `remark_message` varchar(512) DEFAULT NULL,
+  `check_status` tinyint DEFAULT '-1',
+  `checker_id` int DEFAULT NULL,
+  `checker_name` varchar(32) DEFAULT NULL,
+  `check_message` varchar(512) DEFAULT NULL,
+  `check_time` timestamp NULL DEFAULT NULL,
+  `extraAttributes` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -103,6 +308,37 @@ CREATE TABLE `user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `user_group_apply`
+--
+
+DROP TABLE IF EXISTS `user_group_apply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_group_apply` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `user_username` varchar(32) NOT NULL,
+  `user_name` varchar(32) NOT NULL,
+  `apply_group_id` int NOT NULL,
+  `tutor_id` int NOT NULL,
+  `tutor_username` varchar(32) NOT NULL,
+  `tutor_name` varchar(32) NOT NULL,
+  `tutor_check_status` tinyint NOT NULL DEFAULT '-1',
+  `manager_check_status` tinyint NOT NULL DEFAULT '-1',
+  `status` tinyint NOT NULL DEFAULT '1',
+  `message_tutor` varchar(300) DEFAULT NULL,
+  `message_manager` varchar(300) DEFAULT NULL,
+  `tutor_check_time` timestamp NULL DEFAULT NULL,
+  `manager_chack_time` timestamp NULL DEFAULT NULL,
+  `manaher_checker_id` int DEFAULT NULL,
+  `manager_checker_name` varchar(32) DEFAULT NULL,
+  `create_time` timestamp NOT NULL,
+  `extraAttributes` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `user_permission`
 --
 
@@ -119,6 +355,31 @@ CREATE TABLE `user_permission` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `week_usage_bill`
+--
+
+DROP TABLE IF EXISTS `week_usage_bill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `week_usage_bill` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `wall_time` int NOT NULL,
+  `gwall_time` int NOT NULL,
+  `fee` decimal(18,2) NOT NULL,
+  `pay_fee` decimal(18,2) NOT NULL DEFAULT '0.00',
+  `start_time` timestamp NOT NULL,
+  `end_time` timestamp NOT NULL,
+  `pay_flag` tinyint NOT NULL DEFAULT '0',
+  `pay_time` timestamp NULL DEFAULT NULL,
+  `pay_type` tinyint DEFAULT NULL,
+  `pay_message` varchar(256) DEFAULT NULL,
+  `extraAttributes` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -129,4 +390,4 @@ CREATE TABLE `user_permission` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-02-12 17:31:29
+-- Dump completed on 2022-02-14 17:44:05
