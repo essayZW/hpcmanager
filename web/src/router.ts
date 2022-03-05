b169d7ed9a94b0be77359b0be7a898715f21f76f
essay <essaybeihai@gmail.com>
2022-03-05 22:06:45 +0800

    Add: 初始化login页面以及登录判断

--- web/src/router.ts
+++ web/src/router.ts
@@ -1,14 +1,16 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import { isInstall } from './service/sys';
+import { IsLogin } from './service/user';
 
 import MainView from './pages/MainView.vue';
 import NotFound from './pages/NotFound.vue';
 import InstallView from './pages/InstallView.vue';
+import LoginView from './pages/LoginView.vue';
 
 const Router: RouteRecordRaw[] = [
   {
     path: '/',
-    name: 'MainView',
+    name: 'Main',
     component: MainView,
   },
   {
@@ -23,6 +25,21 @@ const Router: RouteRecordRaw[] = [
           message: '已经安装,跳转到登录界面',
         });
         return '/login';
+      }
+    },
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+    beforeEnter: async () => {
+      // 判断是否登录
+      const userInfo = await IsLogin();
+      if (userInfo != null) {
+        ElMessage({
+          message: '已经登录,跳转到主页面',
+        });
+        return '/';
       }
     },
   },
--- web/src/service/user.ts
+++ web/src/service/user.ts
@@ -1,3 +1,5 @@
+import { ApiRequest } from '../api/api';
+
 // 创建用户参数
 export interface CreateUserParam {
   username: string;
@@ -6,4 +8,31 @@ export interface CreateUserParam {
   tel: string;
   email: string;
   collegeName: string;
+}
+
+// 已经登录的用户的基本信息
+export interface LoginUserInfo {
+  userID: number;
+  username: string;
+  userName: string;
+  groupID: number;
+  levels: number[];
+}
+
+// 判断用户是否已经登录,如果已经登录,返回登录的用户的信息
+export async function IsLogin(): Promise<LoginUserInfo | null> {
+  try {
+    const { status, data } = await ApiRequest.request(
+      '/user/token',
+      'GET',
+      {},
+      {}
+    );
+    if (!status) {
+      return null;
+    }
+    return data as LoginUserInfo;
+  } catch (error) {
+    return null;
+  }
 }
