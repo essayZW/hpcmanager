fd11328c0596232d27883719769ed3d0e87ae988
essay <essaybeihai@gmail.com>
2022-03-05 10:46:06 +0800

    Fix: 格式

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,8 +18,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        dir: "../proxy/build/web",
-      }
-    }
-  }
-})
+        dir: '../proxy/build/web',
+      },
+    },
+  },
+});
