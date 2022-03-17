7aa13b5acb7729633944f041b15283fee9e7e95b
essay <essaybeihai@gmail.com>
2022-03-17 20:20:21 +0800

    Init: node服务

---
+++ node/docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sed -i s/host:\ 127.0.0.1/host:\ $MYSQL_HOST/g /config-template.yaml
+sed -i s/port:\ 3306/port:\ $MYSQL_PORT/g /config-template.yaml
+sed -i s/database:\ hpcmanager/database:\ $MYSQL_DATABASE/g /config-template.yaml
+sed -i s/username:\ mysqlroot/username:\ $MYSQL_USERNAME/g /config-template.yaml
+sed -i s/password:\ mysqlpass/password:\ $MYSQL_PASSWORD/g /config-template.yaml
+sed -i s/address:\ etcdaddress/address:\ $ETCD_ADDRESS/g /config-template.yaml
+
+
+
+mkdir -p /root/.config/hpcmanager
+cp /config-template.yaml /root/.config/hpcmanager/config-$HPCMANAGER_ENV.yaml
+
+# run app
+/main
---
+++ project/README.md
@@ -0,0 +1,3 @@
+# project
+
+project 服务,主要提供对于系统中的项目(project)信息的管理查看服务
