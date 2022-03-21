b9f72b6b11fe4239e4274a6090b2d92612306b4a
essay <essaybeihai@gmail.com>
2022-03-21 15:36:41 +0800

    Fix: 审核用户加入组申请时候验证申请信息还未撤销

--- user/db/user_group_apply.go
+++ user/db/user_group_apply.go
@@ -174,8 +174,7 @@ func (ugadb *UserGroupApplyDB) QueryByID(ctx context.Context, applyID int) (*Use
 
 // UpdateTutorCheckStatus 更新导师审核状态
 func (ugadb *UserGroupApplyDB) UpdateTutorCheckStatus(ctx context.Context, newStatus *UserGroupApply) (bool, error) {
-	// FIXME: 验证请求还未撤销
-	res, err := ugadb.db.Exec(ctx, "UPDATE `user_group_apply` SET `tutor_check_status`=?, `message_tutor`=?,`tutor_check_time`=? WHERE `id`=?",
+	res, err := ugadb.db.Exec(ctx, "UPDATE `user_group_apply` SET `tutor_check_status`=?, `message_tutor`=?,`tutor_check_time`=? WHERE `id`=? AND `status`=1",
 		newStatus.TutorCheckStatus, newStatus.MessageTutor, newStatus.TutorCheckTime, newStatus.ID)
 	if err != nil {
 		logger.Warn("UpdateTutorCheckStatus error: ", err)
@@ -190,9 +189,8 @@ func (ugadb *UserGroupApplyDB) UpdateTutorCheckStatus(ctx context.Context, newSt
 
 // UpdateAdminCheckStatus 更新管理员审核状态
 func (ugadb *UserGroupApplyDB) UpdateAdminCheckStatus(ctx context.Context, newStatus *UserGroupApply) (bool, error) {
-	// FIXME: 验证请求还未撤销
 	res, err := ugadb.db.Exec(ctx, "UPDATE `user_group_apply` SET `manager_check_status`=?, `message_manager`=?, `manager_check_time`=?,"+
-		"`manager_checker_id`=?, `manager_checker_username`=?, `manager_checker_name`=? WHERE `id`=? AND `tutor_check_status`!=-1",
+		"`manager_checker_id`=?, `manager_checker_username`=?, `manager_checker_name`=? WHERE `id`=? AND `tutor_check_status`!=-1 AND `status`=1",
 		newStatus.ManagerCheckStatus, newStatus.MessageManager, newStatus.ManagerCheckTime, newStatus.ManagerCheckerID, newStatus.ManagerCheckerUsername,
 		newStatus.ManagerCheckerName, newStatus.ID)
 	if err != nil {
