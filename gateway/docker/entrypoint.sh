ac1c91649f0e57117975c16ed551f2ba7ce3b0bc
essay <essaybeihai@gmail.com>
2022-04-24 22:34:06 +0800

    Fix: docker文件

--- gateway/docker/entrypoint.sh
+++ gateway/docker/entrypoint.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-sed -i s/host:\ 127.0.0.1/host:\ $MYSQL_HOST/g /config-template.yaml
-sed -i s/port:\ 3306/port:\ $MYSQL_PORT/g /config-template.yaml
-sed -i s/database:\ hpcmanager/database:\ $MYSQL_DATABASE/g /config-template.yaml
-sed -i s/username:\ mysqlroot/username:\ $MYSQL_USERNAME/g /config-template.yaml
-sed -i s/password:\ mysqlpass/password:\ $MYSQL_PASSWORD/g /config-template.yaml
 sed -i s/address:\ etcdaddress/address:\ $ETCD_ADDRESS/g /config-template.yaml
-
-
+sed -i s/address:\ 172.17.0.4:6379/address:\ $REDIS_ADDRESS/g /config-template.yaml
 
 mkdir -p /root/.config/hpcmanager
 cp /config-template.yaml /root/.config/hpcmanager/config-$HPCMANAGER_ENV.yaml
+
+sleep 20
 
 # run app
 if [ "$HPCMANAGER_ENV" == "production" ]
--- hpc/docker/entrypoint.sh
+++ hpc/docker/entrypoint.sh
@@ -6,8 +6,8 @@ sed -i s/database:\ hpcmanager/database:\ $MYSQL_DATABASE/g /config-template.yam
 sed -i s/username:\ mysqlroot/username:\ $MYSQL_USERNAME/g /config-template.yaml
 sed -i s/password:\ mysqlpass/password:\ $MYSQL_PASSWORD/g /config-template.yaml
 sed -i s/address:\ etcdaddress/address:\ $ETCD_ADDRESS/g /config-template.yaml
-
-
+sed -i s/address:\ 172.17.0.4:6379/address:\ $REDIS_ADDRESS/g /config-template.yaml
+sed -i s/address:\ 172.17.0.6:5672/address:\ $RABBITMQ_ADDRESS/g /config-template.yaml
 
 mkdir -p /root/.config/hpcmanager
 cp /config-template.yaml /root/.config/hpcmanager/config-$HPCMANAGER_ENV.yaml
